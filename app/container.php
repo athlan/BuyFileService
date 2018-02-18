d2722be5eea6da445a89bc7f208e8fc294428536
Athlan <athlan@vgroup.pl>
2018-02-18 21:56:13 +0100

    add DI framework

---
+++ app/container.php
@@ -0,0 +1,17 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+
+$app = new Application();
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+$app['twig'] = $app->extend('twig', function ($twig, $app) {
+    return $twig;
+});
+
+return $app;
---
+++ web/app.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', 0);
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = require __DIR__.'/../app/container.php';
+
+require __DIR__.'/../app/config/prod.php';
+require __DIR__.'/../app/controllers.php';
+
+$app->run();
